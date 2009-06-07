f8421c58ad934f327b943d1e64baf29551b8bf5f
Daniel Cadenas <dcadenas@gmail.com>
2009-06-06 22:10:11 -0300

    More tests and refactor

--- lib/state_pattern.rb
+++ lib/state_pattern.rb
@@ -2,30 +2,34 @@ require 'facets'
 
 module StatePattern
   def self.included(base)
-    base.class_eval do
-      attr_accessor :state
-      def self.add_states(*state_classes)
+    base.instance_eval do
+      def initial_state
+        @initial_state
+      end
+
+      def set_initial_state(state_class)
+        @initial_state = state_class
+      end
+
+      def add_states(*state_classes)
         state_classes.each do |state_class|
           include state_class
         end
       end
+    end
 
-      def self.set_initial_state(state_class)
-        @@initial_state = state_class
-      end
+    attr_accessor :state
+    def initialize(*args)
+      super(*args)
+      self.state = self.class.initial_state
+    end
 
-      def initialize(*args)
-        super(*args)
-        self.state = @@initial_state
-      end
+    def state_instance
+      as(state)
+    end
 
-      def state_instance
-        as(state)
-      end
-
-      def transition_to(state_module)
-        self.state = state_module
-      end
+    def transition_to(state_module)
+      self.state = state_module
     end
   end
 end
--- test/state_pattern_test.rb
+++ test/state_pattern_test.rb
@@ -1,54 +1,88 @@
 require 'test_helper'
 
-module James
-  protected
+module Family
+  module James
+    protected
 
-  def name
-    transition_to(Lynn)
-    "James #{last_name}"
+    def name
+      transition_to(Lynn)
+      "James #{last_name}"
+    end
+  end
+
+  module Lynn
+    protected
+
+    def name
+      transition_to(James)
+      "Lynn #{last_name}"
+    end
+  end
+
+  class Member
+    include StatePattern
+    add_states James, Lynn
+    set_initial_state Lynn
+
+    def name
+      state_instance.name
+    end
+
+    def last_name
+      "Holbrook"
+    end
   end
 end
 
-module Lynn
-  protected
-
-  def name
-    transition_to(James)
-    "Lynn #{last_name}"
-  end
-end
-
-class FamilyMember
-  include StatePattern
-  add_states James, Lynn
-  set_initial_state Lynn
-
-  def name
-    state_instance.name
+module Button
+  module On
+    def press
+      transition_to Off
+      "off"
+    end
   end
 
-  def last_name
-    "Holbrook"
+  module Off
+    def press
+      transition_to On
+      "on"
+    end
+  end
+
+  class Button
+    include StatePattern
+    add_states On, Off
+    set_initial_state Off
+
+    def press
+      state_instance.press
+    end
   end
 end
 
 Expectations do
   expect "Lynn Holbrook" do
-    member = FamilyMember.new
+    member = Family::Member.new
     member.name 
   end
 
   expect "James Holbrook" do
-    member = FamilyMember.new
+    member = Family::Member.new
     member.name 
     member.name 
   end
 
   expect "Lynn Holbrook" do
-    member = FamilyMember.new
+    member = Family::Member.new
     member.name 
     member.name 
     member.name 
+  end
+
+  expect ["Lynn Holbrook", "on", "James Holbrook", "off"] do
+    button = Button::Button.new
+    member = Family::Member.new
+    [member.name, button.press, member.name, button.press]
   end
 end
 
