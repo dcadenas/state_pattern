4a8ad455149e7e96f80e4500ab36f10e8b9de0a5
Daniel Cadenas <dcadenas@gmail.com>
2009-10-08 22:20:58 -0200

    Some refactorings and small tweaks

--- test/arguments_of_event_delegation_test.rb
+++ test/arguments_of_event_delegation_test.rb
@@ -29,4 +29,16 @@ Expectations do
     sample_class_instance = ::SampleClassWithStatePattern.new
     sample_class_instance.event("arg1", "arg2")
   end
+
+=begin
+  PENDING
+  Currently using class_eval to define the delegation method (so we can pass a block)
+  is causing a segmention fault on 1.8.6 so we only use define_method to do the delegation
+  if you want to pass a block do so as a normal variable, do use yield
+
+  expect "state event args == arg1, arg2, arg3" do
+    sample_class_instance = ::SampleClassWithStatePattern.new
+    sample_class_instance.event("arg1", "arg2"){"arg3"}
+  end
+=end
 end
